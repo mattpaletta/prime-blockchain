099fc4c41e5eb9546efd3cdc8b65fb3812002d65
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2018-10-24 22:59:52 -0700

    UI changes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,48 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+class App extends React.Component<{}, { isView: boolean }> {
+  constructor(props: any){
+    super(props);
+    this.state = { 
+      isView: true,
+    };
+    this.viewCallback = this.viewCallback.bind(this);
+    this.learnCallback = this.learnCallback.bind(this);
+  }
 
-class App extends React.Component {
+  public viewCallback() {
+    this.setState({ isView: true });
+  }
+
+  public learnCallback() {
+    this.setState({ isView: false });
+  }
+
   public render() {
+    const viewElement = document.getElementById('view');
+    const learnElement = document.getElementById('learn');
+
+    if (viewElement && learnElement) {
+      if (this.state.isView) {
+        learnElement.style.textDecoration = 'none';
+        viewElement.style.textDecoration = 'underline';
+      } else {
+        viewElement.style.textDecoration = 'none';
+        learnElement.style.textDecoration = 'underline';
+      }
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div className="heading-div">
+        <div className='parent-div-1'>
+          <div className='child-div'>prime blockchain</div>
+        </div>
+
+        <div className='parent-div-2'>
+          <a id='view' className='child-div' onClick={ this.viewCallback }>view</a>
+          <a id='learn' className='child-div' onClick={ this.learnCallback }>learn</a>
+        </div>
       </div>
     );
   }
